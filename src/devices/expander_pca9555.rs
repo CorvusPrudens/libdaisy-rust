61ac43b6a777ab69aaafb331e68891f2b2ab3da5
Stephen Hensley <shensley.audio@gmail.com>
2026-01-30 12:35:28 -0800

    potentially functional driver after latest changes/cleanup.

--- src/devices/expander_pca9555.rs
+++ src/devices/expander_pca9555.rs
@@ -2,7 +2,8 @@ use crate::hal;
 use hal::i2c::I2c;
 use hal::prelude::*;
 use stm32h7xx_hal::prelude::{
-    _embedded_hal_blocking_i2c_Write as Write, _embedded_hal_blocking_i2c_WriteRead as WriteRead,
+    _embedded_hal_blocking_i2c_Read as Read, _embedded_hal_blocking_i2c_Write as Write,
+    _embedded_hal_blocking_i2c_WriteRead as WriteRead,
 };
 
 /// Data corresponding to a single PCA9555 I/O Expander
@@ -14,7 +15,8 @@ use stm32h7xx_hal::prelude::{
 ///
 /// For now this is a minimal driver for providing access
 /// to 16 additional inptus.
-pub struct Pca9555 {
+pub struct Pca9555<I2CN> {
+    i2c: I2c<I2CN>,
     address: u8,
     states: [bool; 16],
 }
@@ -29,11 +31,15 @@ pub enum I2cError<W, WR> {
 pub type WriteReadError<W: Write + WriteRead> =
     I2cError<<W as Write>::Error, <W as WriteRead>::Error>;
 
-impl Pca9555 {
-    pub fn new<W: Write + WriteRead>(address: u8, bus: &mut W) -> Result<Self, WriteReadError<W>> {
+impl<I2CN> Pca9555<I2CN>
+where
+    I2c<I2CN>: Write + WriteRead + Read,
+{
+    pub fn new(address: u8, mut bus: I2c<I2CN>) -> Self {
         let base_address = 0x20;
-        let dev_addr = base_address + address;
-        let dev = Pca9555 {
+        let dev_addr = base_address | (address & 0x07);
+        let mut dev = Pca9555 {
+            i2c: bus,
             address: dev_addr,
             states: Default::default(),
         };
@@ -44,73 +50,44 @@ impl Pca9555 {
         // polarity = not-inverted
         // So for our initial implementation.. no config is necessary
 
-        // Fill internal data with current states on hardware.
-        dev.update();
+        // Any attempts to write here will cause an infinite loop during if device is not
+        // present and serial callback hasn't been started. So maybe we don't do this here yet.
 
-        Ok(dev)
+        // This was here to test why device wasn't sending ACK (but it wasn't on the PCB ...)
+        // Will remove these comments, and finish driver once I have appropriate HW
+        // crate::delay::CycleDelay::new().delay_ms(30u8);
+        // let _ = dev
+        //     .i2c
+        //     .write(dev.address, &[Register::ConfigurationPort0 as u8, 0xff]);
+
+        // Fill internal data with current states on hardware.
+        // dev.update();
+
+        dev
     }
 
     /// Polls the device, reading both of it's input registers to fill the
     /// internal state data
-    pub fn update<W: Write + WriteRead>(&mut self, bus: &mut W) -> Result<(), WriteReadError<W>> {
+    // pub fn update(&mut self) -> Result<(), WriteReadError<W>> {
+    pub fn update(&mut self) {
         // Register pairs flip automatically after reads.
         // So we can basically just send the `InputPort0` command,
         // and read two data bytes back every time we want new data
-        let mut raw_states = [u8; 2];
-        self.read(bus, Register::InputPort0, raw_states)?;
+        let mut raw_states = [0u8; 2];
+        let _ = self
+            .i2c
+            .write_read(self.address, &[Register::InputPort0 as u8], &mut raw_states);
 
+        let raw_union: u16 = (raw_states[0] as u16) | ((raw_states[1] as u16) << 8);
         for i in 0..16 {
-            let raw_union: u16 = ((raw_states[0] as u16) | ((raw_states as u16) << 8));
             self.states[i] = (raw_union & (1 << i)) != 0;
         }
-        Ok(())
     }
 
     /// Returns the current state recorded for a particular pin
-    pub fn get_state(self, index: u16) -> bool {
-        let idx = index.clamp(0, 16);
+    pub fn get_state(&self, index: usize) -> bool {
+        let idx = index.min(15);
         self.states[idx]
-    }
-
-    fn write<W: Write + WriteRead>(
-        &self,
-        bus: &mut W,
-        register: Register,
-        data: u8,
-    ) -> Result<(), WriteReadError<W>> {
-        self.write_raw(bus, register as u8, data)
-    }
-
-    fn write_raw<W: Write + WriteRead>(
-        &self,
-        bus: &mut W,
-        register: u8,
-        data: u8,
-    ) -> Result<(), WriteReadError<W>> {
-        // This is probably not what we need
-        bus.write(self.address, &[register, data])
-            .map_err(I2cError::Write)?;
-
-        Ok(())
-    }
-
-    fn read<W: Write + WriteRead>(
-        &self,
-        bus: &mut W,
-        register: Register,
-        buffer: &mut [u8],
-    ) -> Result<(), WriteReadError<W>> {
-        self.read_raw(bus, register as u8, buffer)
-    }
-
-    fn read_raw<W: Write + WriteRead>(
-        &self,
-        bus: &mut W,
-        register: u8,
-        buffer: &mut [u8],
-    ) -> Result<(), WriteReadError<W>> {
-        bus.write_read(self.address, &[register], buffer)
-            .map_err(I2cError::WriteRead)
     }
 }
 
